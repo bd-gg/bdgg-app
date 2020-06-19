4fd8891f3719ebf6730034d996112ed2aef09097
Oscarchoi <wychoi502@gmail.com>
2020-06-20 03:42:49 +0900

    [feat] Connect setting screen with components

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { View, ScrollView, Text } from 'react-native';
 
+import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
+
+import OptionContainer from '../components/OptionContainer.js';
+import TypographyOption from '../components/TypographyOption.js';
 
 function SettingLayout() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'white',
-      }}
-    />
+    <ScrollView>
+      <View>
+        <OptionContainer title="Accounts" >
+          <TypographyOption name="프로필 변경" onPress={() => { alert("TODO") }} />
+          <TypographyOption name="비밀번호 변경" />
+        </OptionContainer>
+        <OptionContainer title="Policy" >
+          <TypographyOption name="이용약관" />
+        </OptionContainer>
+        <OptionContainer title="Contact" >
+          <TypographyOption name="버그 리포트" />
+          <TypographyOption name="리뷰 작성" />
+        </OptionContainer>
+      </View>
+    </ScrollView>
   );
 }
 
 export default function SettingScreen() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Main" component={SettingLayout} />
+      <Stack.Screen name="Settings" component={SettingLayout} />
     </Stack.Navigator>
   );
 }
