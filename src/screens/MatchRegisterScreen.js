fb9965b82c0a9ecd08a7748bc44587ad69e149a3
Oscarchoi <wychoi502@gmail.com>
2020-07-15 16:23:45 +0900

    [feat] Add ToggleOption component
    - 토글 스위치 옵션 컴포넌트 설계

--- src/screens/MatchRegisterScreen.js
+++ src/screens/MatchRegisterScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 import TextInputContainer from '~/components/TextInputOption';
+import ToggleInputContainer from '~/components/ToggleOption';
 
 import { TouchableOpacity } from "react-native-gesture-handler";
 
@@ -11,6 +12,8 @@ export default class MatchRegisterScreen extends React.Component {
       <View>
         <TextInputContainer icon="ios-trophy" hint="제목"></TextInputContainer>
         <TextInputContainer icon="ios-calendar" hint="일정"></TextInputContainer>
+        <ToggleInputContainer icon="ios-trophy" title="체크 옵션"></ToggleInputContainer>
+
         <TouchableOpacity>
           <View style={{ backgroundColor: "#0175DF", alignItems: "center", borderRadius: 10, padding: 10 }}>
             <Text style={{ color: "white", fontSize: 20 }}>추가</Text>
